1a03faeb1ac9921f1d381e3dbabdb623737d71ad
b.enders@hochhuth.de <b.enders@hochhuth.de>
2023-10-24 21:33:34 +0200

    changed some typesin db

--- src/services/types.ts
+++ src/services/types.ts
@@ -2,8 +2,9 @@ export interface Equivalent {
   id: string;
   name: string;
   comment: null | string;
-  unit: string;
+  year: number;
   avgValue: number;
+  monthlyValues: boolean;
   jan: null | number;
   feb: null | number;
   mar: null | number;
@@ -16,26 +17,60 @@ export interface Equivalent {
   oct: null | number;
   nov: null | number;
   dec: null | number;
-  year: number;
-  project: string;
-  source: string;
-  monthlyValues: boolean;
+  parent: null | string; // reference on table equivalents. if set a calculation chain is used
+  source: null | string; // reference on table sources. user inputs can have NULL
+  project: null | string; // reference on table projects. system values will be NULL
+  in: string; // input unit
+  out: string; // output unit
 }
-
-export type Scope = "scope1" | "scope2" | "scope3";
 
 export interface UserInput {
   id: string;
-  scope: Scope;
   name: string;
+  scope: number; // 1-3
+  comment: string;
   rawValue: number;
   sumValue: number;
-  equivalent: null | string;
-  project: string;
-  year: number;
-  comment: string;
+  equivalent: null | string; // reference on table equivalents
+  report: string; // reference on table reports
 }
 
 export interface UserInputQuery {
-  scope?: string;
+  scope?: number;
+}
+
+export interface ReportEntry {
+  project: string; // the project id
+  year: number;
+  companyName: string;
+  companyStreet: string;
+  companyPostal: string;
+  companyCity: string;
+  companyCountry: string;
+  contactName: string;
+  contactTelephone: string;
+  contactEmail: string;
+  contactDomain: string;
+  companyDomain: string;
+  countEmployees: number;
+  businessTurnover: number;
+  baseYear: number;
+  baseEquivalentSource: null | string; // reference on table sources
+}
+
+export interface SourceEntry {
+  id: string;
+  name: string;
+}
+
+export interface ProjectEntry {
+  id: string;
+  name: string;
+}
+
+export interface CategoryEntry {
+  id: string;
+  name: string;
+  parent: null | string; // reference on table categories. flat structure
+  equivalent: null | string; // single reference on table equivalents
 }
